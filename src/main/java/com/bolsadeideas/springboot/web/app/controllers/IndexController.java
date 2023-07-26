d7d41cb8fc8a14db290e8e0fc46f089fd53aa2fb
v4sh6n <128129211+v4sh6n@users.noreply.github.com>
2023-07-26 19:28:32 +0200

    canbios

--- src/main/java/com/bolsadeideas/springboot/web/app/controllers/IndexController.java
+++ src/main/java/com/bolsadeideas/springboot/web/app/controllers/IndexController.java
@@ -1,13 +1,15 @@
 package com.bolsadeideas.springboot.web.app.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class IndexController {
 	
 	@GetMapping({"/index","/","/home"})
-	public String index() {
+	public String index(Model model) {
+		model.addAttribute("titulo", "SpringBoot FRamework");
 		
 		return "index";
 	}
