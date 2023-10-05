995c92ac64716648ef6f53081cbaae7431edd21c
v4sh6n <128129211+v4sh6n@users.noreply.github.com>
2023-10-05 18:05:34 +0200

    Notación @ModelAttribute

--- src/main/java/com/bolsadeideas/springboot/web/app/controllers/IndexController.java
+++ src/main/java/com/bolsadeideas/springboot/web/app/controllers/IndexController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.bolsadeideas.springboot.web.app.models.Usuario;
@@ -39,15 +40,27 @@ public class IndexController {
 	
 	@RequestMapping("/listar")
 	public String listar(Model model) {
+//		List<Usuario> usuarios = new ArrayList<>();
+//		usuarios.add(new Usuario("Nombre","Apellido","email"));
+//		usuarios.add(new Usuario("Johna","Doe","email2"));
+//		usuarios.add(new Usuario("Jane","Doe","email3"));
+//		usuarios.add(new Usuario("Tornado","Roe","email4"));
+//		model.addAttribute("titulo", "Listado de usuarios: ");
+//		model.addAttribute("usuarios", usuarios);
+		
+		return "listar";
+	}
+	
+	@ModelAttribute("usuarios")
+	public List<Usuario> poblarUsuarios(){
 		List<Usuario> usuarios = new ArrayList<>();
 		usuarios.add(new Usuario("Nombre","Apellido","email"));
 		usuarios.add(new Usuario("Johna","Doe","email2"));
 		usuarios.add(new Usuario("Jane","Doe","email3"));
 		usuarios.add(new Usuario("Tornado","Roe","email4"));
-		model.addAttribute("titulo", "Listado de usuarios: ");
-		model.addAttribute("usuarios", usuarios);
 		
-		return "listar";
+		return usuarios;
+		
 	}
 
 }
